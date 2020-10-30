cb2010e2c50fff6db4d63403105d8039ce98fd8c
ravikjha7 <ravikjha7@gmail.com>
2020-10-30 19:45:11 +0530

    Array started

---
+++ array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{   int n,i,sum=0;
+    int arr[n];
+    printf("Enter size of Array:\n");
+    scanf("%d",&n);
+    printf("Enter %d elements\n",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    for(i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    printf("Sum of all elements of the array is : %d\n",sum);
+    
+
+
+}
